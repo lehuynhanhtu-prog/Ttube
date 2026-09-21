55333d9b88ec9fc2c56e3da2eb5bce1dcb918fa1
lehuynhanhtu-prog <lehuynhanhtu@gmail.com>
2026-09-21 10:29:27 +0700

    Fix Media3 library service types

--- app/src/main/java/vn/ttube/audio/TtubeLibraryService.kt
+++ app/src/main/java/vn/ttube/audio/TtubeLibraryService.kt
@@ -5,7 +5,8 @@ import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaLibraryService.LibraryParams
-import androidx.media3.session.MediaLibrarySession
+import androidx.media3.session.MediaLibraryService.LibraryResult
+import androidx.media3.session.MediaLibraryService.MediaLibrarySession
 import androidx.media3.session.MediaSession
 import com.google.common.collect.ImmutableList
 import com.google.common.util.concurrent.Futures
